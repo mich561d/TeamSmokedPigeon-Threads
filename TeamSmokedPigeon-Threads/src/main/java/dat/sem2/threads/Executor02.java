4dc0db83042bbfb1f58b8c06c2c771bffcdf6dea
Michael <mich561d@hotmail.com>
2018-08-28 11:42:49 +0200

    Opgave 4

--- TeamSmokedPigeon-Threads/src/main/java/dat/sem2/threads/Executor02.java
+++ TeamSmokedPigeon-Threads/src/main/java/dat/sem2/threads/Executor02.java
@@ -8,7 +8,7 @@ public class Executor02 {
     public static void main(String[] args) {
         ExecutorService workingJack = Executors.newSingleThreadExecutor();
         for (int count = 0; count < 25; count++) {
-            final String s = "Hello " + count + " to us";
+            String s = "Hello " + count + " to us";
             workingJack.submit(() -> {
                 // Det er en rød opgave at forklare hvad denne fejl skyldes
                 // Fjern udkommenteringen i næste linje
