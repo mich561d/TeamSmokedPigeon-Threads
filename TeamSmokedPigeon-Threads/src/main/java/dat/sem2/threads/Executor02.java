92ae3e2df1f2d4b27a6c6669bde956badbde1365
Michael <mich561d@hotmail.com>
2018-08-28 11:40:25 +0200

    Opgave 4

--- TeamSmokedPigeon-Threads/src/main/java/dat/sem2/threads/Executor02.java
+++ TeamSmokedPigeon-Threads/src/main/java/dat/sem2/threads/Executor02.java
@@ -3,17 +3,17 @@ package dat.sem2.threads;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-
 public class Executor02 {
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         ExecutorService workingJack = Executors.newSingleThreadExecutor();
-        for ( int count = 0; count < 25; count++ ) {
-            workingJack.submit( () -> {
+        for (int count = 0; count < 25; count++) {
+            final String s = "Hello " + count + " to us";
+            workingJack.submit(() -> {
                 // Det er en rød opgave at forklare hvad denne fejl skyldes
                 // Fjern udkommenteringen i næste linje
-                //System.out.println( "Hello "+ count + " to us" );
-            } );
+                System.out.println(s);
+            });
         }
         workingJack.shutdown();
     }
