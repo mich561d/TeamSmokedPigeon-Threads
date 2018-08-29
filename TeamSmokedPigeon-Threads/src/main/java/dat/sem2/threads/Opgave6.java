f92b06750592958aae9eb0a004c75ad3c7435dc7
Christian <rygerlunger@gmail.com>
2018-08-29 10:24:33 +0200

    Opgave 1 fredag a)

--- TeamSmokedPigeon-Threads/src/main/java/dat/sem2/threads/Opgave6.java
+++ TeamSmokedPigeon-Threads/src/main/java/dat/sem2/threads/Opgave6.java
@@ -1,7 +1,14 @@
 package dat.sem2.threads;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Opgave6 {
 
@@ -14,4 +21,40 @@ public class Opgave6 {
 
     }
 
+}
+
+class FileRunner implements Runnable {
+
+    String root;
+    String path;
+    String html;
+    String httpResponse;
+    Socket socket;
+
+    public FileRunner(String root, String path, Socket socket) throws Exception {
+        this.root = root;
+        this.path = path;
+        this.socket = socket;
+        html = getResourceFileContents(root + path);
+        httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + html;
+    }
+
+    @Override
+    public void run() {
+        try {
+            socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+        } catch (IOException ex) {
+            Logger.getLogger(FileRunner.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private static String getResourceFileContents(String fileName) throws Exception {
+        //Get file from resources folder
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        URL url = classLoader.getResource(fileName);
+        File file = new File(url.getFile());
+        String content = new String(Files.readAllBytes(file.toPath()));
+        return content;
+
+    }
 }
